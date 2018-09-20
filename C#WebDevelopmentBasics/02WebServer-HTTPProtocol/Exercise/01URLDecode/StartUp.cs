a6070eac2515f80c13888be3eb710a8f95577d7e
Slavyana <slaviana95@gmail.com>
2018-09-20 16:30:23 +0300

    Web Server - HTTP Protocol

---
+++ C#WebDevelopmentBasics/02WebServer-HTTPProtocol/Exercise/01URLDecode/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _01URLDecode
+{
+    using System;
+    using System.Net;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var inputUrl = Console.ReadLine();
+            var decodedInput = WebUtility.UrlDecode(inputUrl);
+            Console.WriteLine(decodedInput);
+        }
+    }
+}
