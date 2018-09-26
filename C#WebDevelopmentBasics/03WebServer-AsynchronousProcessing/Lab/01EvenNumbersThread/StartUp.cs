77ac90921fd33f38efca861e4a407b9b266de675
Slavyana <slaviana95@gmail.com>
2018-09-26 23:24:29 +0300

    Web Server - Asynchronous Processing Lab

---
+++ C#WebDevelopmentBasics/03WebServer-AsynchronousProcessing/Lab/01EvenNumbersThread/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _01EvenNumbersThread
+{
+    using System;
+    using System.Linq;
+    using System.Threading;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] range = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int start = range[0];
+            int end = range[1];
+
+            Thread evens = new Thread(() => PrintEvenNumbers(start, end));
+            evens.Start();
+            evens.Join();
+            Console.WriteLine("Thread finished work");
+        }
+
+        private static void PrintEvenNumbers(int start, int end)
+        {
+            int firstEven = start % 2 == 0 ? start : ++start;
+            for (int even = firstEven; even <= end; even += 2)
+            {
+                Console.WriteLine(even);
+            }
+        }
+    }
+}
