1edae34cef9b51dbea1d541e5c8aba1828c88435
Slavyana <slaviana95@gmail.com>
2018-10-24 17:24:15 +0300

    Listing followed channels on Users' Home page

--- C#WebDevelopmentBasics/SIS.Niki.Mvc/Apps/MishMashApp/Controllers/HomeController.cs
+++ C#WebDevelopmentBasics/SIS.Niki.Mvc/Apps/MishMashApp/Controllers/HomeController.cs
@@ -2,19 +2,47 @@
 {
     using SIS.HTTP.Responses.Contracts;
     using SIS.MvcFramework.Attributes;
+    using System.Linq;
+    using Models.Enums;
+    using ViewModels.Channel;
 
     public class HomeController : BaseController
     {
         [HttpGet("/home/index")]
         public IHttpResponse Index()
         {
+            if (this.User != null)
+            {
+                var user = this.DbContext.Users.FirstOrDefault(u => u.Username == this.User);
+
+                if (user.Role == UserRole.User)
+                {
+                    var followedChannels = this.DbContext.Channels
+                        .Where(c => c.Followers.Any(f => f.User.Username == this.User))
+                        .Select(c => new FollowedChannelViewModel
+                        {
+                            Id = c.Id,
+                            Name = c.Name,
+                            Type = c.Type,
+                            FollowersCount = c.Followers.Count(),
+                        }).ToArray();
+
+                    return this.View("UserHomeIndex", followedChannels);
+                }
+                else
+                {
+                    return this.View("AdminHomeIndex");
+                }
+            }
+
             return this.View("HomeIndex");
+
         }
 
         [HttpGet("/")]
         public IHttpResponse RootIndex()
         {
-            return this.View("HomeIndex");
+            return this.Index();
         }
     }
 }
