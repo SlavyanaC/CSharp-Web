4274f6c24f7e9829aacbfde7828da97048c23857
Slavyana <slaviana95@gmail.com>
2018-10-24 13:29:27 +0300

    Fixed bug in ViewEngine

--- C#WebDevelopmentBasics/SIS.Niki.Mvc/SIS.MvcFramework/ViewEngine/ViewEngine.cs
+++ C#WebDevelopmentBasics/SIS.Niki.Mvc/SIS.MvcFramework/ViewEngine/ViewEngine.cs
@@ -15,7 +15,7 @@
     {
         public string GetHtml<T>(string viewName, string viewCode, T model, string user = null)
         {
-            var viewTypeName = viewName + "View";
+            var viewTypeName = viewName.Replace("/", "_") + "View";
             var csharpMethodBody = this.GenerateCSharpMethodBody(viewCode);
             var viewCodeAsCSharpCode = @"
             using System;
