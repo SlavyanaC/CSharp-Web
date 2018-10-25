e36913baa818157eef1852015d185673579b797d
Slavyana <slaviana95@gmail.com>
2018-10-25 14:00:14 +0300

    Delete unused code

--- C#WebDevelopmentBasics/SIS/SIS.Framework/Views/View.cs
+++ C#WebDevelopmentBasics/SIS/SIS.Framework/Views/View.cs
@@ -12,48 +12,5 @@
         }
 
         public string Render() => this.fullHtmlContent;
-
-        //private readonly IDictionary<string, object> viewData;
-
-        //public View(string fullHtmlContent, IDictionary<string, object> viewData)
-        //{
-        //    this.fullHtmlContent = fullHtmlContent;
-        //    this.viewData = viewData;
-        //}
-
-        //public string Render()
-        //{
-        //    var fullHtml = this.ReadFile();
-        //    var renderedHtml = this.RenderHtml(fullHtml);
-
-        //    return renderedHtml;
-        //}
-
-        //private string ReadFile()
-        //{
-        //    if (!File.Exists(this.fullHtmlContent))
-        //    {
-        //        throw new FileNotFoundException($"File {fullHtmlContent} not found");
-        //    }
-
-        //    return File.ReadAllText(this.fullHtmlContent);
-        //}
-
-        //private string RenderHtml(string fullHtml)
-        //{
-        //    if (this.viewData.Any())
-        //    {
-        //        foreach (var parameter in this.viewData)
-        //        {
-        //            var dynamicDataPlaceholder = $"{{{{{{{parameter.Key}}}}}";
-        //            if (fullHtml.Contains(dynamicDataPlaceholder))
-        //            {
-        //                fullHtml = fullHtml.Replace(dynamicDataPlaceholder, parameter.Value.ToString());
-        //            }
-        //        }
-        //    }
-
-        //    return fullHtml;
-        //}
     }
 }
