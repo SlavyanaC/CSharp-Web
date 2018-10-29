dc9d13adf6aaa49f80518b2bed5b62c75d0553ac
Slavyana <slaviana95@gmail.com>
2018-10-29 14:10:04 +0200

    Deleted InlineResourceResult and improved TextResult

--- C#WebDevelopmentBasics/SIS.Niki.Mvc/SIS.WebServer/Results/TextResult.cs
+++ C#WebDevelopmentBasics/SIS.Niki.Mvc/SIS.WebServer/Results/TextResult.cs
@@ -7,10 +7,10 @@
 
     public class TextResult : HttpResponse
     {
-        public TextResult(string content, HttpResponseStatusCode responseStatusCode)
+        public TextResult(string content, HttpResponseStatusCode responseStatusCode, string contentType = "text/plain; charset=utf-8")
             : base(responseStatusCode)
         {
-            this.Headers.Add(new HttpHeader("Content-Type", "text/plain; charset=uft-8"));
+            this.Headers.Add(new HttpHeader(HttpHeader.ContentType, contentType));
             this.Content = Encoding.UTF8.GetBytes(content);
         }
     }
