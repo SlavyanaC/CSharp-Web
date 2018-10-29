00ef8e2a3234f4640049911171faef34ef55e6f7
Slavyana <slaviana95@gmail.com>
2018-10-29 23:02:56 +0200

    Delete files

---
+++ C#WebDevelopmentBasics/SIS/SIS.Demo/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿namespace SIS.Demo.Controllers
+{
+    using SIS.Framework.Controllers;
+    using Framework.ActionResults.Contracts;
+    using Framework.Attributes.Action;
+    using Framework.Security;
+
+    public class UsersController : Controller
+    {
+        public IActionResult Login()
+        {
+            this.SignIn(new IdentityUser { Username = "Pesho", Password = "123" });
+            return this.View();
+        }
+
+        [Authorize]
+        public IActionResult Authorized()
+        {
+            this.Model["username"] = this.Identity.Username;
+            return this.View();
+        }
+    }
+}
