3ec6a42d6fba884de7654edb2bae1fd3f0e66ff3
Slavyana <slaviana95@gmail.com>
2018-11-01 14:27:32 +0200

    Implemented user functionality in ChushkaWebApp

---
+++ C#WebDevelopmentBasics/SIS.Niki.Mvc/Apps/ChushkaWebApp/ViewModels/Users/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ChushkaWebApp.ViewModels.Users
+{
+   public class LoginViewModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
