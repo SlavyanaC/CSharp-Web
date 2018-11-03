900565b8a64e04839c5b94ec973f0fcb375a5c36
Slavyana <slaviana95@gmail.com>
2018-11-03 11:40:39 +0200

    IServiceCollection typo

---
+++ C#WebDevelopmentBasics/SIS.Niki.Mvc/SIS.MvcFramework/Services/Contracts/IServiceCollection.cs
@@ -0,0 +1,15 @@
+﻿namespace SIS.MvcFramework.Services.Contracts
+{
+    using System;
+
+    public interface IServiceCollection
+    {
+        void AddService<TSource, TDestination>();
+
+        T CreateInstance<T>();
+
+        object CreateInstance(Type type);
+
+        void AddService<T>(Func<T> p);
+    }
+}
